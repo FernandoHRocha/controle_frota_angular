19e8a093a2761b28648942129874f58165e896fa
FernandoHRocha <fhrlobacz@gmail.com>
2021-11-12 13:40:45 -0300

    .

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,5 @@
 const https = require('https')
 const fs = require('fs')
-
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('test.json')
